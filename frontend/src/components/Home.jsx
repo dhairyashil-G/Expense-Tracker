f5fe58f5c7026fd53f9582f819b72580673b724d
awaze7 <awazeshaikh7@gmail.com>
2023-04-27 19:59:24 +0530

    Home Page created

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,31 @@
 function Home() {
-  return <h2>Hi, I am a Car!</h2>;
-}
-
+  const features = [
+    { text: 'Record your expenses and categorize them', symbol: '💰' },
+    { text: 'View a summary of your spending', symbol: '📊' },  
+    { text: 'Analyze your expenses with charts and graphs', symbol: '📈' },
+    { text: 'Set budget goals and track your progress', symbol: '🎯' },
+  ];
+  
+    const fancyOneLine = 'Master your finances with ease and style!';
+  
+    return (
+      <div className="bg-gray-100 min-h-screen">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="py-12 text-center">
+            <h1 className="text-4xl font-bold text-blue-700">Expense Tracker</h1>
+            <p className="mt-4 text-lg text-gray-600">{fancyOneLine}</p>
+            <div className="mt-12 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+              {features.map((feature, index) => (
+                <div key={index} className="flex flex-col items-center bg-white rounded-lg p-4 shadow-lg">
+                  <span className="text-3xl mb-2">{feature.symbol}</span>
+                  <span className="text-lg text-gray-700">{feature.text}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 export default Home;
